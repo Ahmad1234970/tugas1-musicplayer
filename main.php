55e82755f6c1397762ebe24cc7db884057110327
ahmad1234970 <ahmadraynaldi88@gmail.com>
2020-06-19 20:07:15 +0700

    upload pertama

---
+++ main.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+
+<link rel="stylesheet" type="text/css" href="<?php echo ASSET; ?>css/style.css">
+<center><h2>Selamat Datang Para Pecinta Music</h2></center>
+<h2 align="center"> Welcome <?php echo $_SESSION['email']; ?></h2>
+</html>
